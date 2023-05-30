5d81ca5acfd53e23c650a8ccf03b9f7939bab0b9
Daniel Nelson <codemrnelson@gmail.com>
2023-05-30 15:44:56 -0400

    Convert Signup into a React Component

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header/Header";
 import About from "./components/About/About";
+import Signup from "./components/Signup/Signup";
 
 export default function App() {
     return (
         <div>
             <Header />
             <About />
+            <Signup />
         </div>
     )
 }
---
+++ src/js/components/Signup/Signup.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Signup() {
+    return (
+        <section className="py-3 pi-1" id="smoke">
+            <div className="container grid grid-gc--3 m-auto ai-c gap-3">
+            <header className="grid-span--full">
+                <h2 className="h-header ta-c">Your Turn</h2>
+                <p className="h-main h-subheader ta-c mb-3">Puff Puff Pass</p>
+            </header>
+            <figure className="section-img-container">
+                <img src="images/section-img-1.jpg" alt="THC infused tea" className="img" />
+            </figure>
+            <div className="section-container">
+                <form action="https://www.freecodecamp.com/email-submit" id="form" className="form ta-c">
+                <p className="p-main mb-3">
+                    A friend with weed is a friend indeed! Sign up to receive news
+                    about the latest news in the cannibis industry.
+                </p>
+                <label htmlFor="email" className="p-main">Email:
+                    <input type="email" name="email" id="email" placeholder="Enter your email address" className="p-1 mb-2 disp-block m-auto" required /></label>
+                <input type="submit" defaultValue="Submit" className="btn btn-primary m-auto" id="submit" />
+                </form>
+            </div>
+            <figure className="section-img-container">
+                <img src="images/section-img-1.jpg" alt="THC infused tea" className="img" />
+            </figure>
+            </div>
+        </section>
+    )
+}
